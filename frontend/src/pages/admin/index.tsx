218b9609a980f03a81208bf4f7f07826139d4b82
Tony Sandro <tonysduarte101@gmail.com>
2025-05-12 00:38:25 -0300

    feat: adding AddCourse in admin page

--- frontend/src/pages/admin/index.tsx
+++ frontend/src/pages/admin/index.tsx
@@ -1,10 +1,11 @@
+import AddCourse from "../../components/adminPage/addCourse/addCourse";
 import InitialAdminSection from "../../components/adminPage/initialAdminSection/initialAdminSection";
 
 function AdminPage() {
   return (
     <div>
       <InitialAdminSection />
-      {/* <AddCourse /> */}
+      <AddCourse />
     </div>
   );
 }
