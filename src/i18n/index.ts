be2e3d59e928f62d9b6abdaa7cdf9840447cf6a7
Tony Sandro <tonysduarte101@gmail.com>
2025-05-12 18:05:47 -0300

    feat:adding persistence in localStorage to i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,20 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from './locales/en/translation.json';
-import pt from './locales/pt/translation.json';
+import en from "./locales/en/translation.json";
+import pt from "./locales/pt/translation.json";
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'pt',
-    interpolation: {
-      escapeValue: false,
-    },
-    resources: {
-      en: { translation: en },
-      pt: { translation: pt },
+    fallbackLng: "pt",
+    interpolation: { escapeValue: false },
+    resources: { pt: { translation: pt }, en: { translation: en } },
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
     },
   });
 
