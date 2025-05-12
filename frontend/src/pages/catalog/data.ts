065948c76a285f7b4fc13694d5d1fcf2d3e55120
Tony Sandro <tonysduarte101@gmail.com>
2025-05-11 21:30:21 -0300

    feat: adding allCourses section in catalog page

---
+++ frontend/src/pages/catalog/data.ts
@@ -0,0 +1,23 @@
+export const CourseObject = {
+  id: 1,
+  title: "JavaScript Basics",
+  description:
+    "Learn the fundamentals of JavaScript, the most popular programming language in web development.",
+  price: 49.99,
+  imageUrl:
+    "https://miro.medium.com/v2/resize:fit:786/format:webp/1*LyZcwuLWv2FArOumCxobpA.png",
+  meta: "by Fulano | Iniciante",
+  createdAt: "2023-01-01",
+  updateAt: "2023-01-01",
+};
+
+export const CourseObject1 = {
+  id: 2,
+  title: "Advanced CSS",
+  description:
+    "Master the art of CSS and learn how to create stunning web pages with advanced techniques.",
+  price: 59.99,
+  imageUrl:
+    "https://kinsta.com/wp-content/uploads/2024/01/wp-advanced-css-techniques-1024x536.jpg",
+  createdAt: "2023-02-01",
+};
--- frontend/src/pages/catalog/index.tsx
+++ frontend/src/pages/catalog/index.tsx
@@ -1,10 +1,13 @@
 import bannerImage from '../../image/education-banner.jpg';
-import InitialSection from '../../components/InitialPage/InitialSection/InitialSection';
+import InitialSection from '../../components/InitialPage/initialSection/InitialSection';
+import { CourseObject, CourseObject1 } from './data';
+import AllCourses from '../../components/InitialPage/allCourses/allCourses';
 
 function Catalog() {
     return (
         <div>
             <InitialSection isAuthenticated={true} bannerImage={bannerImage} />
+            <AllCourses courses={[CourseObject, CourseObject1]} />
         </div>
     );
 }
